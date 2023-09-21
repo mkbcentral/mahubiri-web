bee0f0386317c79b3f26902e9b8c2c172ef19436
mkbcentral <mkbcentralcode@gmail.com>
2023-09-21 11:52:05 +0200

    Make crud church repository

---
+++ app/Repositories/Church/CrudChurchRepository.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace App\Repositories;
+
+use App\Models\Church;
+
+class CrudChurchRepository{
+    public static function create(array $inputs):Church{
+        return Church::create($inputs);
+    }
+    public static function show(string $id):Church{
+        return Church::find($id);
+    }
+    public static function uupdate(Church $church,array $inputs):Church{
+         $church->update($inputs);
+         return $church;
+    }
+    public static function delete(Church $church):bool{
+        if ($church->delete()) {
+           return true;
+        }
+    }
+}
