956d6658092f704a855ad907cdec92ad0f129011
mkbcentral <mkbcentralcode@gmail.com>
2023-09-21 11:55:59 +0200

    Make crud preaching repository

---
+++ app/Repositories/Church/CrudPreachingRepository.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace App\Repositories;
+
+use App\Models\Preaching;
+
+class CrudPreachingRepository{
+    public static function create(array $inputs):Preaching{
+        return Preaching::create($inputs);
+    }
+    public static function show(string $id):Preaching{
+        return Preaching::find($id);
+    }
+    public static function uupdate(Preaching $preaching,array $inputs):Preaching{
+         $preaching->update($inputs);
+         return $preaching;
+    }
+    public static function delete(Preaching $preaching):bool{
+        if ($preaching->delete()) {
+           return true;
+        }
+    }
+}
