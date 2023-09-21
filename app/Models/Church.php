5422c80ce54c70201cc9bcdeae61e2bbd9a4c67f
mkbcentral <mkbcentralcode@gmail.com>
2023-09-21 09:58:37 +0200

    Add hasMany preachings realionship to Church model

--- app/Models/Church.php
+++ app/Models/Church.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Church extends Model
 {
     use HasFactory;
     protected $fillable=['name','abreviation','phone','email','logo_url'];
+    /**
+     * Get all of the preachings for the Church
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function preachings(): HasMany
+    {
+        return $this->hasMany(Preaching::class);
+    }
 }
