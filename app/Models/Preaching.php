cdca1d44cb059792868af0da6ef8ac9ad1bc7a13
mkbcentral <mkbcentralcode@gmail.com>
2023-09-21 11:21:19 +0200

    Add belongsTo to Preaching model relationShip

--- app/Models/Preaching.php
+++ app/Models/Preaching.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Preaching extends Model
 {
     use HasFactory;
     protected $fillable=['title','preacher_name','preaching_url','church_id'];
-    
+    /**
+     * Get the church that owns the Preaching
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function church(): BelongsTo
+    {
+        return $this->belongsTo(Church::class, 'church_id');
+    }
+
 }
